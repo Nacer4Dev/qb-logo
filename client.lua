9c7078290be20058607e811a67dd224b6a33167b
Nacer / <nacerlachi5@gmail.com>
2025-07-10 03:32:42 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,17 +1,14 @@
--- دالة لعرض الشعار
 function ShowLogo()
-    -- إرسال رسالة إلى واجهة المستخدم لعرض الشعار
     SendNUIMessage({
         type = "showLogo",
         enable = true
     })
 end
 
--- تشغيل الدالة عند بدء اللعبة
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        ShowLogo() -- عرض الشعار
-        break -- إيقاف التكرار بعد العرض الأول
+        ShowLogo() 
+        break 
     end
 end)
